e7df2aa094efe014d5037bbe8db1aa7c8d10ebbf
freyesft <f.reyesft@gmail.com>
2024-09-14 11:17:11 -0500

    [ADD] terms

---
+++ stock-flow-db/inserts/add-units-measurement.sql
@@ -0,0 +1,15 @@
+-- TERMS GROUP
+INSERT INTO
+    terms_group (id, name)
+VALUES
+(1, 'Units of measurement') -- TERMS
+INSERT INTO
+    terms (id, terms_group_id, term)
+VALUES
+(101, 1, 'Unidad'),
+    (102, 1, 'Caja'),
+    (103, 1, 'Kilogramo (kg)'),
+    (104, 1, 'Litro (L)'),
+    (105, 1, 'Metro (m)'),
+    (106, 1, 'Pares'),
+    (107, 1, 'Libra (lb)')
